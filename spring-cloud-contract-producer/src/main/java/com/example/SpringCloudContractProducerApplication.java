dd858481d8305a0b4a58d950e07bf2087e4811de
Zoltan Altfatter <altfatterz@gmail.com>
2019-02-18 22:22:56 +0100

    inital commit

---
+++ spring-cloud-contract-producer/src/main/java/com/example/SpringCloudContractProducerApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudContractProducerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudContractProducerApplication.class, args);
+	}
+
+}
